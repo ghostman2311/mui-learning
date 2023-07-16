021dccca4bc8d6e08c7c345fcd867bd1012913cb
nikku <nkarnwal07@gmail.com>
2023-07-16 14:20:25 +0530

    appbar transitions

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core";
+
+function ElevationScroll(props) {
+  const { children } = props;
+
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
+function Header() {
+  return (
+    <ElevationScroll>
+      <AppBar position="fixed">
+        <Toolbar>Arc Development</Toolbar>
+      </AppBar>
+    </ElevationScroll>
+  );
+}
+
+export { Header };
