fb569e7fa1d39b72b5c833d6aabfb9f4e4c3bfa8
nikku <nkarnwal07@gmail.com>
2023-07-16 15:47:07 +0530

    themeprovider added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core";
+import { AppBar, Toolbar, useScrollTrigger } from "@mui/material";
 
 function ElevationScroll(props) {
   const { children } = props;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "@material-ui/styles";
+import { theme } from "./components/theme";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
